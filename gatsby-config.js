3f68f143b0e17c7f7fdfcb1ccefefeb94ae2700e
mydeadpixel <mydeadpixel@users.noreply.github.com>
2019-12-23 19:35:26 +0000

    added

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,11 +8,25 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-transformer-remark`,
-    `gatsby-plugin-prefetch-google-fonts`,
     {
       resolve: `gatsby-source-datocms`,
       options: {
         apiToken: process.env.DATO_API_TOKEN,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Oswald`,
+            subsets: [`300`, `600`],
+          },
+          {
+            family: `Playfair Display`,
+            variants: [`400`, `700`]
+          },
+        ],
       },
     },
   ],
