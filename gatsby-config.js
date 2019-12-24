40cad014e68586d5ce97d3ebe0b66b2ebbd1cae4
mydeadpixel <mydeadpixel@users.noreply.github.com>
2019-12-24 10:52:06 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,20 @@ module.exports = {
         apiToken: process.env.DATO_API_TOKEN,
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Oswald`,
+            subsets: [`300`, `600`],
+          },
+          {
+            family: `Playfair Display`,
+            variants: [`400`, `700`]
+          },
+        ],
+      },
+    },
   ],
 }
