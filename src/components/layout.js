32eca3857416f1570fcffb0b7cd3a0f0536c0602
mydeadpixel <mydeadpixel@users.noreply.github.com>
2019-12-25 21:51:45 +0000

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -49,7 +49,7 @@ const TemplateWrapper = ({ children }) => (
       <div className="container__sidebar">
         <div className="sidebar">
           <h6 className="sidebar__title">
-            <Img {data.datoCmsHome.logo.fluid} />
+            <Img fluid={data.datoCmsHome.logo.fluid} />
           </h6>
           <div
             className="sidebar__intro"
